03327e1242dd48b7a1dee551e1a37375fec95ee7
R99bbit <rjsdid7325@gmail.com>
2021-11-02 12:59:35 +0900

    e4 account update and bvp data extraction

--- app.py
+++ app.py
@@ -19,34 +19,8 @@ def convert_unixtime(date_time):
 
 def convert_csv_to_json(root_path):
     session_csv_path    = f'{root_path}'
-    session_acc_path    = f'{root_path}/ACC.csv'
     session_bvp_path    = f'{root_path}/BVP.csv'
-    session_eda_path    = f'{root_path}/EDA.csv'
-    session_hr_path     = f'{root_path}/HR.csv'
-    session_ibi_path    = f'{root_path}/IBI.csv'
-    session_temp_path   = f'{root_path}/TEMP.csv'
-    session_tags_path    = f'{root_path}/tags.csv'
     DATA = dict()
-    
-    ''' TEMP.csv '''
-    csvfile = open(session_temp_path, 'r')
-    fieldnames = ['temperature(C)']
-    data = list(csv.reader(csvfile))
-    csvfile = [[float(d[0]) for d in data]]
-    s = dict()
-    for i in range(len(fieldnames)):
-        s[fieldnames[i]] = csvfile[i]
-    DATA['TEMP'] = s
-
-    ''' EDA.csv '''
-    csvfile = open(session_eda_path, 'r')
-    fieldnames = ["microsecond"]
-    data = list(csv.reader(csvfile))
-    csvfile = [[float(d[0]) for d in data]]
-    s = dict()
-    for i in range(len(fieldnames)):
-        s[fieldnames[i]] = csvfile[i]
-    DATA['EDA'] = s
 
     ''' BVP.csv '''
     csvfile = open(session_bvp_path, 'r')
@@ -59,75 +33,11 @@ def convert_csv_to_json(root_path):
         s[fieldnames[i]] = csvfile[i]
     DATA['BVP'] = s
 
-    ''' ACC.csv '''
-    csvfile = open(session_acc_path, 'r')
-    fieldnames = ["x", "y", "z"]
-    data = list(csv.reader(csvfile))
-    csvfile = []
-    for i in range(len(fieldnames)):
-        temp = [float(d[i]) for d in data]
-        csvfile.append(temp)
-    
-    s = dict()
-    for i in range(len(fieldnames)):
-        s[fieldnames[i]] = csvfile[i]
-    DATA['ACC'] = s
-
-    ''' IBI.csv '''
-    csvfile = open(session_ibi_path, 'r')
-    fieldnames = ["sec", "duration-in-sec"]
-    data = list(csv.reader(csvfile))
-    csvfile = []
-
-    if(os.stat(session_ibi_path).st_size == 0):
-        s = dict()
-    else:
-        for i in range(len(fieldnames)):
-            if i==1:
-                temp = [data[0][1]]+[float(d[i]) for d in data[1:]]
-            else:
-                temp = [float(d[i]) for d in data]
-            csvfile.append(temp)
-        
-        s = dict()
-        for i in range(len(fieldnames)):
-            s[fieldnames[i]] = csvfile[i]
-
-    DATA['IBI'] = s
-
-    ''' HR.csv '''
-    if(os.stat(session_hr_path).st_size == 0):
-        s = dict()
-    else:
-        csvfile = open(session_hr_path, 'r')
-        fieldnames = ['initial-time-of-the-session', 'sample-rate(Hz)', "BVP"]
-        data = list(csv.reader(csvfile))
-        
-        csvfile = [float(data[0][0]), float(data[1][0]), [float(d[0]) for d in data[2:]]]
-                
-        s = dict()
-        for i in range(len(fieldnames)):
-            s[fieldnames[i]] = csvfile[i]
-    DATA['HR'] = s
-
-    ''' tags '''
-    if(os.stat(session_tags_path).st_size == 0):
-        s = dict()
-    else:
-        csvfile = open(session_tags_path, 'r')
-        fieldnames = ["event-time"]
-        csvfile = [[float(d[0]) for d in data]]
-            
-        s = dict()
-        for i in range(len(fieldnames)):
-            s[fieldnames[i]] = csvfile[i]
-    DATA['tags'] = s
-
     return DATA
 
 ''' API '''
 USER = 'uso699@naver.com'
-PASS = 'gimdodo4564'
+PASS = 'qkrals1216!'
 obj = E4Connect(user=USER, pwd=PASS)
 app = Flask(__name__)
 api = Api(app)
